ef6e430f8cea6a309134529e5ea4612e732e3f8d
Mayka <163645840+MaykaXX@users.noreply.github.com>
2024-03-19 17:45:00 +0200

    Test commit. Started writing the code for the popup window after registration

--- app/src/androidTest/java/com/example/e_diary/Sign_up.java
+++ app/src/androidTest/java/com/example/e_diary/Sign_up.java
@@ -1,25 +1,58 @@
 package com.example.e_diary;
 
 
-import android.support.v4.app.RemoteActionCompatParcelizer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Sign_up extends AppCompatActivity {
+    private EditText nickname;
+    private EditText email;
+    private Button login;
 
-    EditText nickname;
-    EditText email;
-    Button login;
+
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+
+        nickname = findViewById(R.id.editTextTextEmailAddress);
+        email = findViewById(R.id.editTextTextEmailAddress);
+        login = findViewById(R.id.button2);
+
+        //when you press the button "login"
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String enteredNickname = nickname.getText().toString();
+                String enteredEmail = email.getText().toString();
+
+            }
+        });
+    }
+        public void btnClick(View v) {
+
+
+        }
+
+        private void showInfo(String text) {
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+        }
+
     }
 
-}
+
+
+
+
+
+
